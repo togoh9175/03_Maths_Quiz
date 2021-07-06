6f7e58f30311e5fa4f525f0a6a4edabfd2dc71a0
togoh9175 <78572366+togoh9175@users.noreply.github.com>
2021-07-06 23:05:20 +0000

    bvro

--- 01_MQ_Base_Component_v02.py
+++ 01_MQ_Base_Component_v02.py
@@ -47,13 +47,103 @@ print()
 
 # Functions go here
 
-import random
+# Functions go here
+def check_rounds():
+    while True:
+        response = input("How many questions: ")
 
-num_1 = random.randint(1, 10)
-num_2 = random.randint(1, 10)
+        round_error = "Please type either <enter> " \
+                      "or an integer that is more than 0\n"
 
-question = "{} + {}".format(num_1, num_2)
-user_ans = int(input("{} = ".format(question)))
-answer = eval(question)
+        # if infinite mode not chosen, check response
+        # is an integer that is more than 0
+        if response != "":
+            try:
+                response = int(response)
 
-print(answer) 
+                # if response is too low, go back to
+                # start of loop
+                if response < 1:
+                    print(round_error)
+                    continue
+            except ValueError:
+                print(round_error)
+                continue
+        return response
+
+
+def choice_checker(question, valid_list, error):
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question).lower()
+
+        # iterates through list and if response is an item
+        # in the list (or the first letter of an item), the
+        # full item name is returned
+
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+                # output error if item not in list
+
+        print(error)
+        print()
+# main routine goes here
+
+# list of valid responses
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+# ask user if they have played before.
+# if 'yes', show instructions
+
+
+# ask user for # of rounds then loop
+rounds_played = 0
+
+rounds_lost = 0
+rounds_drawn = 0
+
+game_summary = []
+
+# Ask user for # of rounds, <enter> for infinite mode
+rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+    # Start of Game Play Loop
+    # Rounds Heading
+    print()
+    if rounds == "":
+        heading = "continuous Mode:  "  "Round {}".format(rounds_played + 1)
+    else:
+        heading = "Round {} of {}".format(rounds_played + 1, rounds)
+    print(heading)
+
+    import random
+
+    num_1 = random.randint(1, 10)
+    num_2 = random.randint(1, 10)
+
+    question = "{} + {}".format(num_1, num_2)
+    user_ans = int(input("{} = ".format(question)))
+    answer = eval(question)
+
+    print(answer) 
+
+    #  ***** rest of loop / game *****
+    print("you choose {}".format(user_ans))
+
+    # output results
+    print(rounds)
+    summary_statement = "Round {}: {}".format(rounds_played + 1, rounds)
+    game_summary.append(summary_statement)
+
+    rounds_played += 1
+
+    # end game if requested # of rounds has been played
+    if rounds_played == rounds:
+        break
